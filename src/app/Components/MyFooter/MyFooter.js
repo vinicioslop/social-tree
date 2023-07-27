9356b366bafb189f7c95d7506e1662c577bfd731
vinicioslop <vinicioslop7@hotmail.com>
2023-07-27 17:20:52 -0300

    mexe alguns parametros pra resolver tamanho de componentes

--- src/app/Components/MyFooter/MyFooter.js
+++ src/app/Components/MyFooter/MyFooter.js
@@ -2,7 +2,7 @@ import styles from './styles.module.css';
 
 export default function MyFooter() {
     return (
-        <div className={styles.footer}>
+        <div className={styles.container}>
             <p>Made by vinicioslop - Copyright &copy; 2023</p>
         </div>
     )
